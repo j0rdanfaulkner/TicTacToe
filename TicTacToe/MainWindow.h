95917faed2708a76724b5320bc39e0b533afc37c
Jordan Faulkner <jfaulkner2j@icloud.com>
2023-01-06 15:39:17 +0000

    Add project files.

---
+++ TicTacToe/MainWindow.h
@@ -0,0 +1,68 @@
+#pragma once
+
+namespace TicTacToe {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Summary for MainWindow
+	/// </summary>
+	public ref class MainWindow : public System::Windows::Forms::Form
+	{
+	public:
+		MainWindow(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: Add the constructor code here
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		~MainWindow()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+
+	private:
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->SuspendLayout();
+			// 
+			// MainWindow
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(9, 18);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(426, 362);
+			this->Font = (gcnew System::Drawing::Font(L"EurostileEF", 12, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->Margin = System::Windows::Forms::Padding(5, 4, 5, 4);
+			this->Name = L"MainWindow";
+			this->Text = L"Tic-Tac-Toe";
+			this->ResumeLayout(false);
+
+		}
+#pragma endregion
+	};
+}
